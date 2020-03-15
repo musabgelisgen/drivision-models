5920bcbd2bd602cd9cfe667d025e814d23ea1269
musabgelisgen <mgelisgen97@gmail.com>
2020-03-15 21:44:16 +0300

    sign detection is not working, label_maps added

--- predict.py
+++ predict.py
@@ -4,10 +4,40 @@ from skimage import exposure
 from flask import Flask, request
 import numpy as np
 import cv2
+import tensorflow as tf
+from object_detection.utils import label_map_util
+import warnings
+from matplotlib import pyplot as plt
+from PIL import Image
+import glob as glob
+import sys
+from object_detection.utils import visualization_utils as vis_util
 
 sign_recognition_model = None
 sign_recognition_label_names = None
 app = Flask(__name__)
+
+
+def run_sign_detection_model():
+    path_to_ckpt = 'models/sign_detection/saved_model.pb'
+    path_to_labels = 'models/gtsdb3_label_map.pbtxt'
+    num_classes = 3
+
+    import tensorflow.compat.v1 as tf
+    tf.disable_v2_behavior()
+
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(path_to_ckpt, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+
+    label_map = label_map_util.load_labelmap(path_to_labels)
+    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=num_classes, use_display_name=True)
+    category_index = label_map_util.create_category_index(categories)
+    print(label_map)
 
 
 def load_sign_recognition_model():
@@ -44,5 +74,6 @@ def process_sign_recognition(image):
 
 
 if __name__ == '__main__':
-    load_sign_recognition_model()  # load model at the beginning once only
-    app.run(debug=True, host='0.0.0.0', port=83)
+    # load_sign_recognition_model()  # load model at the beginning once only
+    run_sign_detection_model()
+    app.run(debug=True, host='0.0.0.0', port=84)
